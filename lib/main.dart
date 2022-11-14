cc43631887bd3c78f4cda3844c2c5233cb44db74
Oleg Litasov <oleg.litasov@dynevo.org>
2022-11-14 14:29:24 +0300

    add download manager stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const UploadFileWidget());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class UploadFileWidget extends StatelessWidget {
+  const UploadFileWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text(
-      'Hello flutter',
-      textDirection: TextDirection.ltr,
-      textAlign: TextAlign.center,
-      style: TextStyle(
-          color: Colors.red, fontSize: 50, fontWeight: FontWeight.bold),
-    ));
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.indigo,
+            floatingActionButton: const FloatingActionButton(
+                onPressed: null, child: Icon(Icons.cloud_download)),
+            appBar: AppBar(
+              title: const Text('Upload manager'),
+              centerTitle: true,
+            ),
+            body: Center(
+                child: Container(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        LinearProgressIndicator(value: 23),
+                        Text(
+                          '23 %',
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        Text(
+                          'Press download button',
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )
+                      ],
+                    )))));
   }
 }
