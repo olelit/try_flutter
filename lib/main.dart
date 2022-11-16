1180e242162c3e78d7bad5bc8ffca5d7a5d8f56f
Oleg Litasov <oleg.litasov@dynevo.org>
2022-11-17 00:00:41 +0300

    add counter for homework

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,87 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(const UploadFileWidget());
+void main() => runApp(const CountWidget());
+
+class CountWidget extends StatelessWidget {
+  const CountWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          fontFamily: 'Roboto',
+          textTheme: Theme.of(context).textTheme.apply(
+                bodyColor: Colors.white,
+              )),
+      home: Scaffold(
+        backgroundColor: Colors.deepPurpleAccent,
+        appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.indigo,
+            title: const Text('Counter')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              Text('Tab "-" to decrement'),
+              CountStatefulWidget(),
+              Text('Tab "+" to increment'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CountStatefulWidget extends StatefulWidget {
+  const CountStatefulWidget({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CountWidgetState();
+  }
+}
+
+class _CountWidgetState extends State<CountStatefulWidget> {
+  int value = 50;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      width: 120,
+      decoration: const BoxDecoration(
+          color: Colors.white70,
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  value++;
+                });
+              },
+              icon: const Icon(Icons.add)),
+          Text(
+            '$value',
+            style: const TextStyle(color: Colors.black),
+          ),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  value--;
+                });
+              },
+              icon: const Icon(Icons.remove)),
+        ],
+      ),
+    );
+  }
+}
 
 class UploadFileWidget extends StatefulWidget {
   const UploadFileWidget({super.key});
@@ -75,5 +155,37 @@ class _UploadFileWidgetState extends State<UploadFileWidget> {
         }
       });
     });
+  }
+}
+
+class AssetImage extends StatelessWidget {
+  const AssetImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Assets',
+            style: TextStyle(fontFamily: 'Roboto'),
+          ),
+        ),
+        body: Center(
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Image.asset('assets/images/index.jpg'),
+              Image.asset('assets/icons/4691465_flutter_icon.png'),
+              const Positioned(
+                  top: 16,
+                  left: 115,
+                  child: Text('TEST TEXT',
+                      style: TextStyle(fontSize: 50, color: Colors.red)))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
